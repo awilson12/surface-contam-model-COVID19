73549062c7f47c0c87f09effe92cb0a6148ad45e
awilson12 <apfeifer@email.arizona.edu>
2020-06-11 20:12:55 -0700

    adding more parameters and scenarios into sensitivity analysis

--- prelim_sensitivity.R
+++ prelim_sensitivity.R
@@ -5,9 +5,10 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)    
 
 
-SIM <- c("DPLPNS", "DPLPS","DPHPNS","DPHPS",
-         "DOLPNS","DOLPS","DOHPNS","DOHPS",
-         "DGLPNS","DGLPS","DGHPNS","DGHPS","control")   
+SIM <- c("DPLPNS", "DPLPS","DPHPNS","DPHPS", "DPallNS", "DPallS",
+         "DOLPNS","DOLPS","DOHPNS","DOHPS", "DOallNS", "DOallS",
+         "DGLPNS","DGLPS","DGHPNS","DGHPS", "DGallNS", "DGallS",
+         "control")   
 
 NUM.SIM <- length(SIM)     # Count the number of iterations for the automated simulations
 
@@ -19,19 +20,25 @@ for(j in 1:NUM.SIM){
   if(sim.name=="DPLPS"){if(dir.exists("DPLPS")==FALSE){dir.create("DPLPS"); setwd("DPLPS")}else{setwd("DPLPS")}}
   if(sim.name=="DPHPNS"){if(dir.exists("DPHPNS")==FALSE){dir.create("DPHPNS"); setwd("DPHPNS")}else{setwd("DPHPNS")}}
   if(sim.name=="DPHPS"){if(dir.exists("DPHPS")==FALSE){dir.create("DPHPS"); setwd("DPHPS")}else{setwd("DPHPS")}}
+  if(sim.name=="DPallNS"){if(dir.exists("DPallNS")==FALSE){dir.create("DPallNS"); setwd("DPallNS")}else{setwd("DPallNS")}}
+  if(sim.name=="DPallS"){if(dir.exists("DPallS")==FALSE){dir.create("DPallS"); setwd("DPallS")}else{setwd("DPallS")}}
   
   if(sim.name=="DOLPNS"){if(dir.exists("DOLPNS")==FALSE){dir.create("DOLPNS"); setwd("DOLPNS")}else{setwd("DOLPNS")}}
   if(sim.name=="DOLPS"){if(dir.exists("DOLPS")==FALSE){dir.create("DOLPS"); setwd("DOLPS")}else{setwd("DOLPS")}}
   if(sim.name=="DOHPNS"){if(dir.exists("DOHPNS")==FALSE){dir.create("DOHPNS"); setwd("DOHPNS")}else{setwd("DOHPNS")}}
   if(sim.name=="DOHPS"){if(dir.exists("DOHPS")==FALSE){dir.create("DOHPS"); setwd("DOHPS")}else{setwd("DOHPS")}}
+  if(sim.name=="DOallNS"){if(dir.exists("DOallNS")==FALSE){dir.create("DOallNS"); setwd("DOallNS")}else{setwd("DOallNS")}}
+  if(sim.name=="DOallS"){if(dir.exists("DOallS")==FALSE){dir.create("DOallS"); setwd("DOallS")}else{setwd("DOallS")}}
   
   if(sim.name=="DGLPNS"){if(dir.exists("DGLPNS")==FALSE){dir.create("DGLPNS"); setwd("DGLPNS")}else{setwd("DGLPNS")}}
   if(sim.name=="DGLPS"){if(dir.exists("DGLPS")==FALSE){dir.create("DGLPS"); setwd("DGLPS")}else{setwd("DGLPS")}}
   if(sim.name=="DGHPNS"){if(dir.exists("DGHPNS")==FALSE){dir.create("DGHPNS"); setwd("DGHPNS")}else{setwd("DGHPNS")}}
   if(sim.name=="DGHPS"){if(dir.exists("DGHPS")==FALSE){dir.create("DGHPS"); setwd("DGHPS")}else{setwd("DGHPS")}}
+  if(sim.name=="DGallNS"){if(dir.exists("DGallNS")==FALSE){dir.create("DGallNS"); setwd("DGallNS")}else{setwd("DGallNS")}}
+  if(sim.name=="DGallS"){if(dir.exists("DGallS")==FALSE){dir.create("DGallS"); setwd("DGallS")}else{setwd("DGallS")}}
   
   if(sim.name=="control"){if(dir.exists("control")==FALSE){dir.create("control"); setwd("control")}else{setwd("control")}}
-
+  
   
   numiterations<-500 
   
@@ -46,8 +53,16 @@ for(j in 1:NUM.SIM){
     Obsframe<-Obs[[i]]
    
     if (i ==1){
+      
+      RNA<-c(mean(!is.na(IVframe$RNAinfectiousall)),mean(!is.na(Roundsframe$RNAinfectiousall)),mean(!is.na(Obsframe$RNAinfectiousall)))
+      SM<-c(mean(IVframe$SMall),mean(Roundsframe$SMall),mean(Obsframe$SMall))
+      TEmouth<-c(mean(IVframe$TE.tempall),mean(Roundsframe$TE.tempall),mean(Obsframe$TE.tempall))
+      Ah.dose<-c(mean(IVframe$AH.doseall),mean(Roundsframe$AH.doseall),mean(Obsframe$AH.doseall))
+      beta.dose<-c(mean(IVframe$beta.dose.all),mean(Roundsframe$beta.dose.all),mean(Obsframe$beta.dose.all))
+      alpha<-c(mean(IVframe$alphaall),mean(Roundsframe$alphaall),mean(Obsframe$alphaall))
       lambda<-c(mean(IVframe$lambda),mean(Roundsframe$lambda),mean(Obsframe$lambda))
       beta<-c(mean(IVframe$beta),mean(Roundsframe$beta),mean(Obsframe$beta))
+      rnainfect<-c(mean(IVframe$RNAinfectiousall),mean(Roundsframe$RNAinfectiousall),mean(Obsframe$RNAinfectiousall))
       duration<-c(mean(IVframe$duration),mean(Roundsframe$duration),mean(Obsframe$duration))
       SH<-c(mean(IVframe$SH),mean(Roundsframe$SH),mean(Obsframe$SH))
       surfconc<-c(mean(IVframe$surfconc),mean(Roundsframe$surfconc),mean(Obsframe$surfconc))
@@ -57,8 +72,15 @@ for(j in 1:NUM.SIM){
       care<-c("IV","Rounds","Obs")
       
     }else{
+      RNAtemp<-c(mean(!is.na(IVframe$RNAinfectiousall)),mean(!is.na(Roundsframe$RNAinfectiousall)),mean(!is.na(Obsframe$RNAinfectiousall)))
+      SMtemp<-c(mean(IVframe$SMall),mean(Roundsframe$SMall),mean(Obsframe$SMall))
+      TEmouthtemp<-c(mean(IVframe$TE.tempall),mean(Roundsframe$TE.tempall),mean(Obsframe$TE.tempall))
+      Ah.dosetemp<-c(mean(IVframe$AH.doseall),mean(Roundsframe$AH.doseall),mean(Obsframe$AH.doseall))
+      beta.dosetemp<-c(mean(IVframe$beta.dose.all),mean(Roundsframe$beta.dose.all),mean(Obsframe$beta.dose.all))
+      alphatemp<-c(mean(IVframe$alphaall),mean(Roundsframe$alphaall),mean(Obsframe$alphaall))
       lambdatemp<-c(mean(IVframe$lambda),mean(Roundsframe$lambda),mean(Obsframe$lambda))
       betatemp<-c(mean(IVframe$beta),mean(Roundsframe$beta),mean(Obsframe$beta))
+      rnainfecttemp<-c(mean(IVframe$RNAinfectiousall),mean(Roundsframe$RNAinfectiousall),mean(Obsframe$RNAinfectiousall))
       durationtemp<-c(mean(IVframe$duration),mean(Roundsframe$duration),mean(Obsframe$duration))
       SHtemp<-c(mean(IVframe$SH),mean(Roundsframe$SH),mean(Obsframe$SH))
       surfconctemp<-c(mean(IVframe$surfconc),mean(Roundsframe$surfconc),mean(Obsframe$surfconc))
@@ -67,6 +89,13 @@ for(j in 1:NUM.SIM){
       infecttemp<-c(max(IVframe$infect),max(Roundsframe$infect),max(Obsframe$infect))
       caretemp<-c("IV","Rounds","Obs")
       
+      RNA<-c(RNA,RNAtemp)
+      SM<-c(SM,SMtemp)
+      TEmouth<-c(TEmouth,TEmouthtemp)
+      Ah.dose<-c(Ah.dose,Ah.dosetemp)
+      beta.dose<-c(beta.dose,beta.dosetemp)
+      alpha<-c(alpha,alphatemp)
+      rnainfect<-c(rnainfect,rnainfecttemp)
       lambda<-c(lambda,lambdatemp)
       beta<-c(beta,betatemp)
       duration<-c(duration,durationtemp)
@@ -84,11 +113,14 @@ for(j in 1:NUM.SIM){
   if (j==1){
     scenario<-rep(sprintf("%s",sim.name),length(lambda))
     frame<-data.frame(lambda=lambda,beta=beta,duration=duration,SH=SH,surfconc=surfconc,k.sall=k.sall,k.hall=k.hall,
-                      infect=infect,care=care,scenario=scenario)
+                      infect=infect,care=care,scenario=scenario,beta.dose=beta.dose,alpha=alpha,
+                      SM=SM,TEmouth=TEmouth,Ah.dose=Ah.dose,RNA=RNA)
+    
   }else{
     scenariotemp<-rep(sprintf("%s",sim.name),length(lambda))
     frametemp<-data.frame(lambda=lambda,beta=beta,duration=duration,SH=SH,surfconc=surfconc,k.sall=k.sall,k.hall=k.hall,
-                          infect=infect,care=care,scenario=scenariotemp)
+                          infect=infect,care=care,scenario=scenariotemp,beta.dose=beta.dose,alpha=alpha,
+                          SM=SM,TEmouth=TEmouth,Ah.dose=Ah.dose,RNA=RNA)
     frame<-rbind(frame,frametemp)
     print(length(frame$lambda)/(j*1500))
           print(j)
@@ -107,8 +139,11 @@ framecor = subset(frame, select = -c(care,scenario) )
 cormat<-round(cor(framecor,method=c("spearman")),2)
 melted_cormat<-melt(cormat)
 ggplot(data=melted_cormat,aes(x=Var1,y=Var2,fill=value))+geom_tile()+
-  geom_text(aes(label = round(value, 1))) +
+  geom_text(aes(label = signif(value, 2))) +
   scale_fill_gradient(low = "white", high = "blue") 
+
+cor(frame$infect,frame$beta.dose,method="spearman")
+cor(frame$infect,frame$alpha,method="spearman")
 
 
 setwd(this.dir)
@@ -117,41 +152,41 @@ write.csv(frame,'frame_sensitivity.csv')
 
 #--------------working on tile plots to investigate relationships between------------------------------------------
 #---------------input parameters that result in highest infection risks--------------------------------------------------------
-A<-ggplot(frame)+geom_tile(aes(x=round(lambda,3),y=round(beta,3),fill=log10(infect)))+
-  scale_x_continuous(name="Hand-to-Surface Transfer Efficiency")+
-  scale_y_continuous(name="Surface-to-Hand Transfer Efficiency")+
-  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
-  theme_pubr()
+#A<-ggplot(frame)+geom_tile(aes(x=round(lambda,3),y=round(beta,3),fill=log10(infect)))+
+#  scale_x_continuous(name="Hand-to-Surface Transfer Efficiency")+
+#  scale_y_continuous(name="Surface-to-Hand Transfer Efficiency")+
+#  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
+#  theme_pubr()
 
-B<-ggplot(frame)+geom_tile(aes(x=round(lambda,3),y=round(SH,3),fill=log10(infect)))+
-  scale_x_continuous(name="Hand-to-Surface Transfer Efficiency")+
-  scale_y_continuous(name="Fraction of Hand Contact Area")+
-  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
-  theme_pubr()
+#B<-ggplot(frame)+geom_tile(aes(x=round(lambda,3),y=round(SH,3),fill=log10(infect)))+
+#  scale_x_continuous(name="Hand-to-Surface Transfer Efficiency")+
+#  scale_y_continuous(name="Fraction of Hand Contact Area")+
+#  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
+#  theme_pubr()
 
-C<-ggplot(frame)+geom_tile(aes(x=round(lambda,3),y=round(log10(surfconc),1),fill=log10(infect)))+
-  scale_x_continuous(name="Hand-to-Surface Transfer Efficiency")+
-  scale_y_continuous(name=expression("log"[10]*phantom(x)*"Surface Concentration"))+
-  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
-  theme_pubr()
+#C<-ggplot(frame)+geom_tile(aes(x=round(lambda,3),y=round(log10(surfconc),1),fill=log10(infect)))+
+#  scale_x_continuous(name="Hand-to-Surface Transfer Efficiency")+
+#  scale_y_continuous(name=expression("log"[10]*phantom(x)*"Surface Concentration"))+
+#  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
+#  theme_pubr()
 
-D<-ggplot(frame)+geom_tile(aes(x=round(beta,3),y=round(log10(surfconc),1),fill=log10(infect)))+
-  scale_x_continuous(name="Surface-to-Hand Transfer Efficiency")+
-  scale_y_continuous(name=expression("log"[10]*phantom(x)*"Surface Concentration"))+
-  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
-  theme_pubr()
+#D<-ggplot(frame)+geom_tile(aes(x=round(beta,3),y=round(log10(surfconc),1),fill=log10(infect)))+
+#  scale_x_continuous(name="Surface-to-Hand Transfer Efficiency")+
+#  scale_y_continuous(name=expression("log"[10]*phantom(x)*"Surface Concentration"))+
+#  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
+#  theme_pubr()
 
-E<-ggplot(frame)+geom_tile(aes(x=round(SH,3),y=round(log10(surfconc),1),fill=log10(infect)))+
-  scale_x_continuous(name="Fraction of Hand Contact Area")+
-  scale_y_continuous(name=expression("log"[10]*phantom(x)*"Surface Concentration"))+
-  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
-  theme_pubr()
+#E<-ggplot(frame)+geom_tile(aes(x=round(SH,3),y=round(log10(surfconc),1),fill=log10(infect)))+
+#  scale_x_continuous(name="Fraction of Hand Contact Area")+
+#  scale_y_continuous(name=expression("log"[10]*phantom(x)*"Surface Concentration"))+
+#  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
+#  theme_pubr()
 
-G<-ggplot(frame)+geom_tile(aes(x=round(SH,3),y=round(beta,3),fill=log10(infect)))+
-  scale_x_continuous(name="Fraction of Hand Contact Area")+
-  scale_y_continuous(name="Surface-to-Hand Transfer Efficiency")+
-  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
-  theme_pubr()
+#G<-ggplot(frame)+geom_tile(aes(x=round(SH,3),y=round(beta,3),fill=log10(infect)))+
+#  scale_x_continuous(name="Fraction of Hand Contact Area")+
+#  scale_y_continuous(name="Surface-to-Hand Transfer Efficiency")+
+#  scale_fill_continuous(name=expression("log"[10]*phantom(x)*"Infection Risk"))+
+#  theme_pubr()
 
 windows()
 ggarrange(A,B,D,C,E,G,common.legend = TRUE)
@@ -170,6 +205,11 @@ G<-ggplot(frame)+geom_point(aes(x=SH,y=infect,colour=scenario))+
   scale_y_continuous(trans="log10")+theme_pubr()
 H<-ggplot(frame)+geom_point(aes(x=duration,y=infect,colour=scenario))+
   scale_y_continuous(trans="log10")+theme_pubr()
+I<-ggplot(frame)+geom_point(aes(x=beta.dose,y=infect,colour=scenario))+
+  scale_y_continuous(trans="log10")+theme_pubr()
+J<-ggplot(frame)+geom_point(aes(x=alpha,y=infect,colour=scenario))+
+  scale_y_continuous(trans="log10")+theme_pubr()
+
 
 windows()
 ggarrange(A,B,C,D,E,G,H,common.legend = TRUE)
